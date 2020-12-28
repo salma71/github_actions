750f4d13f23d271f9010d67d74f169b3d4a5e0ec
Salma Elshahawy <salmaelshahawy@salmas-mbp-2.myfiosgateway.com>
2020-12-28 08:15:13 -0500

    add .pylintrc config

--- demo.py
+++ demo.py
@@ -1,9 +1,14 @@
-import functools
 from typing import List
 import random
 
-def random_sampling(k: int, A: List[int]) -> None:
+def random_sampling(k: int, array_list: List[int]) -> None:
+    """[summary]
+
+    Args:
+        k (int): [description]
+        A (List[int]): [description]
+    """
     for i in range(k):
-        random_index = random.randint(i, len(A) - 1)
+        random_index = random.randint(i, len(array_list) - 1)
         # swap element based on the generated index
-        A[i], A[random_index] = A[random_index], A[i]
+        array_list[i], array_list[random_index] = array_list[random_index], array_list[i]
